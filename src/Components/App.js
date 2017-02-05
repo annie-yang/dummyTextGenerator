ee454a21f5321e5f53b036b76b21917ac2f679e8
Annie Y <annie_y48@hotmail.com>
2017-02-04 16:49:32 -0800

    Fetching data from API

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,46 @@
+// Modules
 import React, {Component} from 'react';
+import axios from 'axios'; // client to make requests with
+
+// Components
+import Output from './Output';
 
 class App extends Component{
-  componentDidMount(){
-      console.log('React Running...');
+  constructor(props){
+    super(props);
+    this.state = {
+      paras: 4,
+      html: true, // shows p tag
+      text: ''
+    }
+  }
+
+  componentWillMount(){
+    this.getText();
+  }
+
+  /*
+    get request from API
+    take parameters from state
+  */
+  getText(){
+    axios.get('http://hipsterjesus.com/api?paras=' + this.state.paras + '&html=' + this.state.html)
+      .then((response) => {
+        this.setState({
+          text:response.data.text
+        },function(){
+          console.log(this.state);
+        });
+      }).catch((err) => {
+        console.log(err);
+      }); // get request from API
   }
 
   render(){
     return (
       <div>
-        MyApp
+        <h1>Dummy Text Generator</h1>
+        <Output value={this.state.text}/>
       </div>
     );
   }
